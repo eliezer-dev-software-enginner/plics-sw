fd992e01dfa686c5fb8175d5af2349e3d73f793a
eliezer-software-enginner <eliezerassuncaocustodio@gmail.com>
2026-02-20 17:23:15 -0300

    corrigido script de gerar aplicação .deb no linux

--- src/main/java/my_app/Launcher.java
+++ src/main/java/my_app/Launcher.java
@@ -2,6 +2,8 @@ package my_app;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.setProperty("com.sun.javafx.application.controls.id", "plics-sw");
+        System.setProperty("com.sun.javafx.application.controls.name", "plics-sw");
         Main.main(args);
     }
 }
