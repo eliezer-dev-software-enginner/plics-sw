63da1dea2018eb7882e39a5e523fda62c5668b0b
eliezer-software-enginner <93846923+eliezer-dev-software-enginner@users.noreply.github.com>
2026-07-31 15:03:19 -0300

    coordenadas

--- src/main/java/my_app/core/AppRoutes.java
+++ src/main/java/my_app/core/AppRoutes.java
@@ -58,10 +58,10 @@ public class AppRoutes {
     final int MIN_HEIGHT = 500;
 
     final int MEDIUM_WIDTH = 950;
-    final int MEDIUM_HEIGHT = 550;
+    final int MEDIUM_HEIGHT = 530;
 
     final int MAX_WIDTH = 1200;
-    final int MAX_HEIGHT = 650;
+    final int MAX_HEIGHT = 620;
 
 
     public Set<Router.Route> routes() {
@@ -69,9 +69,9 @@ public class AppRoutes {
         return Set.of(
                 new Router.Route(Screens.SPLASH.name(), ctx -> new SplashScreen(),
                         new RouteProps(MIN_WIDTH, MIN_HEIGHT, Main.BASE_TITLE, false)),
-                new Router.Route(Screens.WELCOME.name(), WelcomeScreen::new, new RouteProps(950, 550, Main.BASE_TITLE, true)),
-                new Router.Route(Screens.AUTH.name(), AuthScreen::new, new RouteProps(950, 550, "Seja muito bem vindo", true)),
-                new Router.Route(Screens.HOME.name(), HomeScreen::new, new RouteProps(MAX_WIDTH, 670, Main.BASE_TITLE, true)),
+                new Router.Route(Screens.WELCOME.name(), WelcomeScreen::new, new RouteProps(MEDIUM_WIDTH, MEDIUM_HEIGHT, Main.BASE_TITLE, true)),
+                new Router.Route(Screens.AUTH.name(), AuthScreen::new, new RouteProps(MEDIUM_WIDTH, MEDIUM_HEIGHT, "Seja muito bem vindo", true)),
+                new Router.Route(Screens.HOME.name(), HomeScreen::new, new RouteProps(MAX_WIDTH, MAX_HEIGHT, Main.BASE_TITLE, true)),
                 //new Router.Route("cad-produtos/${id}",ctx-> new ProdutoScreen(ctx), new Router.RouteProps(1500, 900,"Cadastro de produtos", false)),
                 new Router.Route(Screens.PRODUTOS.name(), ProdutoScreen::new, new RouteProps(MAX_WIDTH, MAX_HEIGHT, "Cadastro de produtos", true)),
                 //ok
